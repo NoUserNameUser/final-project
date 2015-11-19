f82b729eb8f54879f4c7be9e9bfef2b0f42cdfa9
reymonmercado <reymonmercado@outlook.com>
2015-11-19 15:22:39 -0800

    deleted examples

--- transmitter.py
+++ transmitter.py
@@ -1,9 +1,8 @@
 import socket
-# comment
+
 # Creates a TCP/IP socket
 socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # Connects as client 
 socket.connect((hostname, portnumber))
 
-#hello
