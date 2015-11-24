3c0cac5f95f490d498d29b0043ca5388c5943816
Jackie Ye <find.jackie@yahoo.com>
2015-11-24 15:39:25 -0800

    changes

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Configuration File
 
 hostnameT = '192.168.0.2'
-hostnameR = 'localhost'
+hostnameR = ''
 hostnameNE = '192.168.0.3'
+rPort = '9000'
--- receiver.py
+++ receiver.py
@@ -5,9 +5,10 @@ import config
 
 print config.hostnameR
 
-# socket = MySocket.mysocket()
-# socket.sock.bind((config.hostnameR, 9000))
-# socket.sock.listen(5)
+socket = MySocket.mysocket()
+socket.sock.bind((config.hostnameR, config.rPort))
+socket.sock.listen(5)
+print "listening on port %s", rPort
 
 # a packet contains type, sequence number, data, window size, ack number
 packet = MyPacket.mypacket('01', 1, 1, 8, None)
