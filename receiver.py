291b9709684f2418537d251ea8c6dfde9ed8f536
Jackie Ye <find.jackie@yahoo.com>
2015-11-24 16:22:36 -0800

    changes

--- receiver.py
+++ receiver.py
@@ -11,11 +11,14 @@ socket.sock.listen(5)
 print "listening on port %s" % config.rPort
 
 while True:
-	socket.sock.revFrom(1024)
-
+	conn, c_adddr = socket.sock.accept()
+	print c_adddr, conn
+	revBuffer = conn.recv(1024)
+	print revBuffer
 	if not data:
 		break
 
 	seqNum = 1
 	ackNum = 0
 	ack = MyPacket.mypacket(1, 1, None, config.windowSize, ackNum)
+	print "%s %s %s %s %s" % ack
