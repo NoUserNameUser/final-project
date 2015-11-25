c75ba87c589aeffcfe95b87ddcaf417f0b42ea0a
reymonmercado <reymonmercado@outlook.com>
2015-11-24 16:02:43 -0800

    edited config and transmitter files

--- transmitter.py
+++ transmitter.py
@@ -4,22 +4,23 @@ import MyPacket
 import MySocket
 import config
 
+# IP Address for Transmitter
+host = config.hostnameT
+
 # Creates a TCP/IP socket
 socket = MySocket.mysocket()
 
-socket.sock.connect(config.hostnameNE)
+socket.sock.connect((config.hostnameNE, config.rPort))
 
 argument = sys.argv
 print argument
-
-
 
 with open(argument[1], 'r') as f:
 	seqNum = 0
 	while True:
 		for i in xrange(8):
 			read_data = f.read(1)
-			packet = MyPacket.mypacket(2, seqNum, read_data, 8, None)
+			packet = MyPacket.mypacket(2, seqNum, read_data, config.windowSize, None)
 			seqNum += 1
 			print read_data
 			socket.sock.sendall(read_data)
