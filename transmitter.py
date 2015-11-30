dff0171895fc0d7cafcaa635934d429a368f487e
Jackie Ye <find.jackie@yahoo.com>
2015-11-29 16:50:27 -0800

    changes

--- transmitter.py
+++ transmitter.py
@@ -9,49 +9,59 @@ import time
 # Creates a TCP/IP socketT
 socketT = MySocket.mysocket()
 
-socketT.sock.connect((config.hostnameNE, config.portA))
+socketT.sock.connect((config.hostnameNE, config.portB))
 
 argument = sys.argv
 print argument
 
 with open(argument[1], 'r') as f:
-	seqNum = 0
-	noAck = False
+	seqNum = 1
+	resend = False
 	cachePacket = []
 	while True:
 		for i in xrange(config.windowSize):
-			print noAck
-			if noAck:
-				print 'in'
+			if resend:
+				# temp = pickle.loads(cachePacket[i])
+				# print "resending packets: %s" % (temp.seqNum)
+				time.sleep(0.001)
 				socketT.sock.sendall(cachePacket[i])
 			else:
-				print 'in else'
+				
+				print 'sending packet. Sequence Number: %s' % (seqNum)
 				read_data = f.read(1)
 				packet = MyPacket.mypacket(2, seqNum, read_data, config.windowSize, None)
 				seqNum += 1
-				# print read_data
+				# encode packet
 				packet = pickle.dumps(packet)
+				# put packets in the cache array
 				cachePacket.append(packet)
-				# print packet
+				# send encoded packet
 				socketT.sock.sendall(packet)
 				
-		# reset noAck variable after for loop
-		noAck = False
+		# reset resend variable after for loop
+		resend = False
 		print '====================', seqNum
 
 		try:
-			socketT.sock.settimeout(1)
+			socketT.sock.settimeout(0.1)
 			ack = socketT.sock.recv(1024)
 		except socket.error, e:
 			err = e.args[0]
 			# if timed out, that means ack is not received
 			if(err == 'timed out'):
-				noAck = True
+				resend = True
 				print "No ACK received, sending same window again"
 				pass
 
-		# time.sleep(1)
-		
+		else:
+			if ack:
+				# decode ack
+				recvAck = pickle.loads(ack)
+				print "ack received, ACK Number: %s" % (recvAck.ackNum)
+
+		if resend == False:
+			# reset cache packet array if resend is false
+			cachePacket = []
 
 		if not read_data:
 			break
